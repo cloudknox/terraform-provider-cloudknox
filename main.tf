78a2c0f783857b2a82a44bb623e826c6dd20c8cd
Saketh Kollu <saketh@cloudknox.io>
2020-05-26 11:03:51 -0700

    Added git ignore

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "cloudknox" {
-
+    service_account_id = "SAU0HMGINBOQNQ4C"
+    access_key = "SAAKBQHF2OGOBD5Q"
+    secret_key = "BCW9qDmvYRQeluTo"
+    shared_credentials_file = "C:\\Users\\Saketh Kollu\\.cnx\\credentials"
 }
 
-resource "cloudknox_policy" "address_policy" {
- address = "1.2.3.4"
+resource "cloudknox_policy" "new-policy" {
+    address = "1"
 }
