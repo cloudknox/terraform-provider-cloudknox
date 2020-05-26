c1f4574eb06d05a0100a916ac226045df9a67d81
Saketh Kollu <saketh@cloudknox.io>
2020-05-26 11:56:44 -0700

    Can make api calls, credentials are hard coded, need to change to parameters

--- common/providerManager.go
+++ common/providerManager.go
@@ -1,8 +1,13 @@
 package common
 
 import (
+	"bytes"
 	"cloudknox/terraform-provider-cloudknox/utils"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"sync"
 
@@ -136,7 +141,7 @@ func setConfiguration(parameters *ClientParameters) (*Client, error) {
 					log.Info("Static Credentials provided, overriding " + configurationType)
 				}
 
-				configurationType = "StaticCredentials"
+				configurationType = "Static Credentials"
 
 				buildClient(parameters.ServiceAccountId, parameters.AccessKey, parameters.SecretKey, configurationType)
 
@@ -184,11 +189,53 @@ func buildClient(sai string, ak string, sk string, configurationType string) {
 	log := GetLogger()
 	log.Info("Using " + configurationType + " to request API Access Token")
 
-	client = &Client{
-		AccessToken: "DUMMY VALUE",
+	// Make POST Request for API Token
+
+	// Setup HTTP Request
+	url := "https://olympus.aws-staging.cloudknox.io/api/v2/service-account/authenticate"
+	var jsonStr = []byte(fmt.Sprintf(`{
+		"serviceAccountId": "%s",
+		"accessKey": "%s",
+		"secretKey": "%s"
+	  }`, sai, ak, sk))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	// Setup Client and Make Request
+	hclient := &http.Client{}
+	resp, err := hclient.Do(req)
+	if err != nil {
+		log.Info(err)
+		client = nil
+		clientErr = errors.New("Client Error")
+		return
+	}
+	defer resp.Body.Close()
+
+	// Get Response
+	log.Println("response Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	jsonBody := string(body)
+
+	// Create Map from Body of Response
+	responseMap := make(map[string]interface{})
+	err = json.Unmarshal([]byte(jsonBody), &responseMap)
+
+	if err != nil {
+		log.Info(err)
+		client = nil
+		clientErr = errors.New("Client Error")
+		return
 	}
 
-	clientErr = errors.New("Client Error")
+	var accessToken = responseMap["accessToken"].(string)
+
+	client = &Client{
+		AccessToken: accessToken,
+	}
+
+	log.Println("Access Token:", client.AccessToken)
 
 	return
 }
@@ -198,6 +245,18 @@ func buildClient(sai string, ak string, sk string, configurationType string) {
 /* Public Functions */
 func GetClient() (*Client, error) {
 	return client, clientErr
+}
+
+func ValidateClient(client *Client) error {
+	if client != nil {
+		if client.AccessToken != "" {
+			return nil
+		}
+
+		return errors.New("No Access Token")
+	}
+
+	return errors.New("No Valid Client")
 }
 
 func SetConfiguration(parameters *ClientParameters) {
